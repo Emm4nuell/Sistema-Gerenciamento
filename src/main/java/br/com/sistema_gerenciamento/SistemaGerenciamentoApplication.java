b5741c5006bba2ead9f5dea5044b068ba0353763
Emm4nuell <eduardo.eesf@gmail.com>
2024-10-05 14:06:26 -0300

    Primeiro commit do desafio

---
+++ src/main/java/br/com/sistema_gerenciamento/SistemaGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistema_gerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistema_gerenciamento/SistemaGerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema_gerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
